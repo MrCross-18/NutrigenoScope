5af1d00fa545a57bed1a4e739473a7b22663bfe9
MrCross-18 <mrcross9421@gmail.com>
2025-01-25 22:51:11 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -95,5 +95,10 @@ def predict():
         "Recommendations": recommendations
     })
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=False) 
+    # Use the port specified by the environment variable, or default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
